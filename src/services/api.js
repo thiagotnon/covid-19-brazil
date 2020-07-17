029bbdd3614cf20a33d33924d5aa0c201dddcb84
yakobus <thiagotnon@gmail.com>
2020-07-17 20:18:02 -0300

    Adiciona variável de ambiente para URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://covid19-brazil-api.now.sh/api/report/v1' });
+const api = axios.create({
+  baseURL: ProcessingInstruction.env.REACT_APP_API_URL
+});
 
 export default api;
