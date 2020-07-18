b7683e6d0975c3b532ea99505b8def3c2a8491a1
yakobus <thiagotnon@gmail.com>
2020-07-17 23:03:13 -0300

    Ajustes de CSS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: 'https://covid19-brazil-api.now.sh/api/report/v1' //process.env.REACT_APP_API_URL
 });
 
 export default api;
