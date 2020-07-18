b21ee4115cd667773880123e612ca13e4d385262
yakobus <thiagotnon@gmail.com>
2020-07-17 22:53:58 -0300

    Adição do link do GitHub

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGlobe, faMap } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 import './styles.css';
 import flag from '../../assets/img/flag.png'
+
 
 const Header = () => (
   <header id="main-header">
@@ -14,6 +16,7 @@ const Header = () => (
    <div className="box-nav">
       <Link className="nav" to={'/'}> <FontAwesomeIcon icon={faMap} />  <span>Todos os Estados</span></Link>
       <Link className="nav" to={'/countries'}> <FontAwesomeIcon icon={faGlobe} /> <span>Países</span></Link>
+      <a className="git" href='https://github.com/thiagotnon/covid-19-brazil' title="GitHub" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faGithub} /></a>
    </div>
     </div>
 
